4fa134e1d25b6f78c7dedcc8ca8f87aceaea1b7c
palisades-lakes <palisades.lakes@gmail.com>
2025-10-06 16:39:25 -0700

    fiddling with wedesoft clj + lwjgl + glfw moon demo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MOP
-meshes (mostly simplicial) and their optimization
+meshes (mostly simplicial), their optimization, and viewers
--- clj.bat
+++ clj.bat
@@ -8,9 +8,7 @@ set GC=
 set TRACE=
 ::set TRACE=-XX:+PrintGCDetails -XX:+TraceClassUnloading -XX:+TraceClassLoading
 
-::set THRUPUT=-d64 -server -XX:+AggressiveOpts 
-::set THRUPUT=-server
-set THRUPUT=
+set UNWARN=--enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow
 
 set XMX=
 ::set XMX=-Xms29g -Xmx29g -Xmn11g 
@@ -21,6 +19,6 @@ set CP=-cp ./src/scripts/clojure;lib/*
 set JAVA="%JAVA_HOME%\bin\java"
 
 set CLJOPTS=-Dclojure.main.report=stderr
-set CMD=%JAVA% %THRUPUT% -ea %GC% %XMX% %TRACE% %OPENS% %CP% %CLJOPTS% clojure.main %* 
+set CMD=%JAVA% %UNWARN% -ea %GC% %XMX% %TRACE% %OPENS% %CP% %CLJOPTS% clojure.main %*
 ::echo %CMD%
 %CMD%
